b5b4dec2180ec816a5421fc476a9003156a79a67
SayZ1X <m3m0ryjunior@gmail.com>
2024-02-24 13:22:46 +0200

    Add My fun

--- app/src/main/java/com/example/packagename_ueedulntucw3/MainActivity.kt
+++ app/src/main/java/com/example/packagename_ueedulntucw3/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.packagename_ueedulntucw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.packagename_ueedulntucw3.ui.theme.PackageNameUeedulntucw3Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyScreen()
                 }
             }
         }
@@ -30,17 +40,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyScreen() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PackageNameUeedulntucw3Theme {
-        Greeting("Android")
-    }
+fun PreviewMyScreen() {
+    MyScreen()
 }
